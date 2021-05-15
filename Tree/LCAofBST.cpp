75ada3c6e06892886373cc046a8864b5c91634bc
Taeho Kim <kim4t@cmich.edu>
2021-05-15 00:32:47 -0400

    Create LCAofBST.cpp

---
+++ Tree/LCAofBST.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        int val = root->val;
+        if(val < p->val && val < q->val)
+        {
+           return lowestCommonAncestor(root->right,p,q);
+        }
+        if(val > q->val && val > p->val)
+        {
+           return lowestCommonAncestor(root->left,p,q);
+        }
+        return root;
+    }
+};
