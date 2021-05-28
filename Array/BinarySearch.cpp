01e3903abb2a6bedb10fbffb6064b3dfe362016e
Taeho Kim <kim4t@cmich.edu>
2021-05-28 15:20:44 -0400

    Update BinarySearch.cpp

--- Array/BinarySearch.cpp
+++ Array/BinarySearch.cpp
@@ -1,3 +1,4 @@
+// recursion
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -17,4 +18,27 @@ public:
         }
         return -1;
     }
+};
+
+
+// loop
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size()-1;
+        while(r>=l)
+        {
+            int mid = (l+r)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target)
+                r = mid-1;
+            else if(nums[mid] < target)
+                l = mid+1;
+        }
+        return -1;
+        
+    }
+    
 };
