725bd37646f8590384bd378438f5609c8fb8b0af
Taeho Kim <kim4t@cmich.edu>
2021-05-30 19:02:07 -0400

    leetcode 56

--- Array/MergeIntervals.cpp
+++ Array/MergeIntervals.cpp
@@ -5,20 +5,22 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
        vector<vector<int>>res;
        sort(intervals.begin(),intervals.end());
-       vector<int>v{intervals[0][0],intervals[0][1]};
+       vector<int>curVec{intervals[0][0],intervals[0][1]};
        
-       for(auto&n : intervals)
+       for(auto&nextVec : intervals)
        {
-          if(n[0] <= v[1])
-              v[1] = max(v[1],n[1]);
+          if(nextVec[0] > curVec[1])
+          {
+              res.push_back(curVec);
+              curVec[0] = nextVec[0];
+              curVec[1] = nextVec[1];
+          }
           else
           {
-              res.push_back(v);
-              v[0] = n[0];
-              v[1] = n[1];
+              curVec[1] = max(nextVec[1],curVec[1]);
           }
        }
-        res.push_back(v);
+        res.push_back(curVec);
         return res;
     }
 };
