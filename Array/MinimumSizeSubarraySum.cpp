0f38ad7245ab1060d5fea88452190b99047c48d9
Taeho Kim <kim4t@cmich.edu>
2021-05-30 15:58:39 -0400

    leetcode 209

--- Array/MinimumSizeSubarraySum.cpp
+++ Array/MinimumSizeSubarraySum.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int sum = 0;
-        int start = 0;
         int res = nums.size()+1;
+        int left = 0;
         for(int i=0;i<nums.size();i++)
         {
             target -= nums[i];
-            while(target<=0)
+            while(target <=0)
             {
-                res = min(res, i-start+1);
-                target += nums[start++];
+                res = min(res,i-left+1);
+                target += nums[left++];
             }
         }
-        return res%(nums.size()+1);
+        return res % (nums.size()+1);
     }
 };
