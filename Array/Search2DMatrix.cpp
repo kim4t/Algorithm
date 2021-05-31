8402d2287cfb4b9d1e9e8be4adc5a642daf69ac4
Taeho Kim <kim4t@cmich.edu>
2021-05-31 17:10:18 -0400

    leetcode 74

---
+++ Array/Search2DMatrix.cpp
@@ -0,0 +1,23 @@
+//leetcode 74
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int left = 0;
+        int right = n*m-1;
+        while(right >= left)
+        {
+            int mid = (left+right)/2;
+            int row = mid/m;
+            int col = mid%m;
+            if(matrix[row][col] == target)
+                return true;
+            else if(matrix[row][col] < target)
+                left = mid+1;
+            else
+                right = mid-1;
+        }
+        return false;
+    }
+};
