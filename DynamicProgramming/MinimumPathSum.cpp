e2528853b103c65e57c8386026278244aef3b501
Taeho Kim <kim4t@cmich.edu>
2021-05-31 22:23:58 -0400

    leetcode 64

---
+++ DynamicProgramming/MinimumPathSum.cpp
@@ -0,0 +1,25 @@
+//leetcode 64
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j>0)
+                    grid[i][j] +=grid[i][j-1];
+                else if(i>0 && j==0)
+                {
+                     grid[i][j] +=grid[i-1][j];
+                }
+                else if(i!=0 && j!=0)
+                {
+                    grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
+                }
+            }
+        }
+        return grid.back().back();
+    }
+};
