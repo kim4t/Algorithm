037b628cf1c60991623f156bffb55e3320ee6a5d
Taeho Kim <kim4t@cmich.edu>
2021-07-13 16:40:24 -0400

    Update GroupAnagrams.cpp

--- String/GroupAnagrams.cpp
+++ String/GroupAnagrams.cpp
@@ -2,31 +2,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> um;
         vector<vector<string>> res;
-        unordered_map<string,vector<int>> um;
         for(int i=0;i<strs.size();i++)
         {
             string s = strs[i];
             sort(s.begin(),s.end());
-            if(um.find(s) != um.end())
-            {
-                um[s].push_back(i);
-            }
-            else
-            {
-                vector<int> v{i};
-                um[s] = v;
-            }
+            um[s].push_back(strs[i]);
         }
-        for(auto vec = um.begin(); vec != um.end(); vec++)
+         for(auto vec = um.begin(); vec != um.end(); vec++)
         {
-            vector<string> v; 
-            for(int i=0;i<vec->second.size();i++)
-            {
-                int idx = vec->second[i];
-                v.push_back(strs[idx]);
-            }
-            res.push_back(v);
+         
+            res.push_back(vec->second);
         }
         return res;
     }
